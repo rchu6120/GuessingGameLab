2eb4ecb94b298f07bf9a5f2021b30d8a3d078573
rchu9700 <rchu9700@bths.edu>
2018-10-12 14:49:42 -0400

    1st objective definitely done

--- src/Objective1.java
+++ src/Objective1.java
@@ -3,19 +3,54 @@ import java.util.Scanner;
 public class Objective1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("What's your name?");
+        System.out.print("What's your name? ");
         String name = input.nextLine();
-        System.out.print("Hey" + name + ", what difficulty would you like? Type 1, Type 2, Type 3, or Type 4?");
+        System.out.print("Hey " + name + ", what difficulty would you like? Type 1 for numbers 1-10, Type 2 for numbers 1-100, Type 3 for numbers 1-1000, or Type 4 for numbers 1-10000? ");
         String type = input.nextLine();
-
-        int n = (int)Math.random()*10+1;
-        int guess = input.nextInt();
         int count = 0;
-        while (guess != (n)) {
-            System.out.print("Guess again!");
-            guess = input.nextInt();
-            count++;
+        if (type.equals("Type 1")) {
+            int n = (int) Math.random() * 10 + 1;
+            System.out.print("Guess my secret number from 1-10! ");
+            int guess = input.nextInt();
+            while (guess != n) {
+                System.out.print("Guess again! ");
+                guess = input.nextInt();
+                count++;
+            }
+            System.out.println("You got it" + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
         }
-        System.out.println("You got it" + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
+        if (type.equals("Type 2")) {
+            int n = (int) Math.random() * 100 + 1;
+            System.out.print("Guess my secret number from 1-100! ");
+            int guess = input.nextInt();
+            while (guess != n) {
+                System.out.print("Guess again! ");
+                guess = input.nextInt();
+                count++;
+            }
+            System.out.println("You got it" + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
+        }
+        if (type.equals("Type 3")) {
+            int n = (int) Math.random() * 1000 + 1;
+            System.out.print("Guess my secret number from 1-1000! ");
+            int guess = input.nextInt();
+            while (guess != n) {
+                System.out.print("Guess again! ");
+                guess = input.nextInt();
+                count++;
+            }
+            System.out.println("You got it" + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
+        }
+        if (type.equals("Type 4")) {
+            int n = (int) Math.random() * 10000 + 1;
+            System.out.print("Guess my secret number from 1-10000! ");
+            int guess = input.nextInt();
+            while (guess != n) {
+                System.out.print("Guess again! ");
+                guess = input.nextInt();
+                count++;
+            }
+            System.out.println("You got it" + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
+        }
     }
 }
