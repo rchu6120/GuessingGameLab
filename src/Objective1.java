b08cbc70c7bb30428a45aae7e1abba13829ab429
Ricky <rchu9700@bths.edu>
2018-10-13 23:33:56 -0400

    gdfdfd

--- src/Objective1.java
+++ src/Objective1.java
@@ -9,7 +9,7 @@ public class Objective1 {
         String type = input.nextLine();
         int count = 0;
         if (type.equals("Type 1")) {
-            int n = (int)(Math.random()*10)+ 1;
+            int n = (int) (Math.random() * 10) + 1;
             System.out.print("Guess my secret number from 1-10! ");
             int guess = input.nextInt();
             while (guess != n) {
@@ -17,10 +17,14 @@ public class Objective1 {
                 guess = input.nextInt();
                 count++;
             }
-            System.out.println("You got it " + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
+            if (count == 1) {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " try." + " The number is " + n + "!");
+            } else {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " tries." + " The number is " + n + "!");
+            }
         }
         if (type.equals("Type 2")) {
-            int n = (int)(Math.random()*100)+ 1;
+            int n = (int) (Math.random() * 100) + 1;
             System.out.print("Guess my secret number from 1-100! ");
             int guess = input.nextInt();
             while (guess != n) {
@@ -28,10 +32,14 @@ public class Objective1 {
                 guess = input.nextInt();
                 count++;
             }
-            System.out.println("You got it " + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
+            if (count == 1) {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " try." + " The number is " + n + "!");
+            } else {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " tries." + " The number is " + n + "!");
+            }
         }
         if (type.equals("Type 3")) {
-            int n = (int)(Math.random()*1000)+ 1;
+            int n = (int) (Math.random() * 1000) + 1;
             System.out.print("Guess my secret number from 1-1000! ");
             int guess = input.nextInt();
             while (guess != n) {
@@ -39,10 +47,14 @@ public class Objective1 {
                 guess = input.nextInt();
                 count++;
             }
-            System.out.println("You got it " + name + "!" + " It took you " + count + " times." + " The number is " + n + "!");
+            if (count == 1) {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " try." + " The number is " + n + "!");
+            } else {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " tries." + " The number is " + n + "!");
+            }
         }
         if (type.equals("Type 4")) {
-            int n = (int)(Math.random()*10000)+ 1;
+            int n = (int) (Math.random() * 10000) + 1;
             System.out.print("Guess my secret number from 1-10000! ");
             int guess = input.nextInt();
             while (guess != n) {
@@ -50,7 +62,11 @@ public class Objective1 {
                 guess = input.nextInt();
                 count++;
             }
-            System.out.println("You got it " + name + "!" + " It took you " + count + " tries." + " The number is " + n + "!");
+            if (count == 1) {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " try." + " The number is " + n + "!");
+            } else {
+                System.out.println("You got it " + name + "!" + " It took you " + count + " tries." + " The number is " + n + "!");
+            }
         }
     }
 }
--- src/Objective2.java
+++ src/Objective2.java
@@ -4,24 +4,27 @@ public class Objective2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Hey computer, guess my number from 1-100!");
-        int computer = (int)(Math.random()*100)+ 1;
-        int computer = input.nextInt();
+        int computer = input.nextInt((int) (Math.random() * 100) + 1);
         int count = 0;
         int min = 1;
         int max = 100;
         String response = input.nextLine();
         if (response.equals("higher")) {
             min = computer;
-            computer = (int)(Math.random()*(100 - computer))+(computer + 1);
+            computer = (int) (Math.random() * (100 - computer)) + (computer + 1);
             count++;
         }
         if (response.equals("lower")) {
             max = computer;
-            computer = (int)(Math.random()*(computer - 1))+1;
+            computer = (int) (Math.random() * (computer - 1)) + 1;
             count++;
         }
         if (response.equals("correct")) {
-            System.out.println("Good job computer! The number is " + computer + ". It took " + count + " tries.");
+            if (count == 1) {
+                System.out.println("Good job computer! The number is " + computer + ". It took " + count + " try.");
+            } else {
+                System.out.println("Good job computer! The number is " + computer + ". It took " + count + " tries.");
+            }
         }
     }
 }
