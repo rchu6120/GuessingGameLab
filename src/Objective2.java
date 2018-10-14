59356639e9dd56c8e6103b1ea0c72b5c6c067e52
Ricky <rchu9700@bths.edu>
2018-10-14 13:41:27 -0400

    done!

--- src/Objective2.java
+++ src/Objective2.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Objective2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Hey computer, guess my number from 1-100! ");
-        int computer =(int)(Math.random() * 100) + 1;
+        System.out.println("Hey computer, guess my number from 1-100! ");
+        sleep(1500);
+        int computer = 50;
         System.out.println(computer);
         int count = 0;
         int min = 1;
@@ -12,24 +13,29 @@ public class Objective2 {
         String response = input.nextLine();
         while (!response.equals("correct")) {
             if (response.equals("higher")) {
-                min = computer;
-                computer = (int)(Math.random() * (max - min)) + (min + 1);
+                min = computer + 1;
+                computer = (int) (Math.random()*(max-min) + min);
                 System.out.println(computer);
-                count++;
             }
             if (response.equals("lower")) {
-                max = computer;
-                computer = (int)(Math.random() * (max - min)) + 1;
+                max = computer - 1;
+                computer = (int) (Math.random()*(max-min) + min);
                 System.out.println(computer);
-                count++;
             }
-            String response = input.nextLine();
+            count++;
+            response = input.nextLine();
         }
         if (count == 1) {
             System.out.println("Good job computer! The number is " + computer + ". It took " + count + " try.");
         } else {
             System.out.println("Good job computer! The number is " + computer + ". It took " + count + " tries.");
         }
+    }
+
+    public static void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (Exception e) {}
     }
 }
 
